5a30f4d908d9080202cfe9b94e0dc62c95e2b6b7
Alina Laniuk <alinavoronina16@gmail.com>
2023-11-27 12:37:41 +0200

    feat: add line component

--- coffee-shop/src/components/beans.js
+++ coffee-shop/src/components/beans.js
@@ -2,10 +2,11 @@ import { Component } from 'react';
 
 import { Block, ColoredBlock } from '../styledComponents/block';
 import { Image } from '../styledComponents/image';
+import Line from './line';
 
 class Beans extends Component {
     render() {
-        const line = <ColoredBlock width='60px' height='2px' background_color={this.props.color} />;
+        const line = <Line width='60px' color={this.props.color} />;
         return (
             <Block
                 width='200px'
---
+++ coffee-shop/src/components/line.js
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+
+import { ColoredBlock } from '../styledComponents/block';
+
+class Line extends Component {
+    render() {
+        const {width, color} = this.props;
+        return <ColoredBlock width={width} height='2px' background_color={color} />;
+    }
+}
+
+export default Line;
